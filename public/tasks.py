bf6732ae3fecdc857495e2e41573bdb39401b300
XiangtingLee <513932815@qq.com>
2020-04-26 00:08:24 +0800

    fix third party login err

--- public/tasks.py
+++ public/tasks.py
@@ -26,13 +26,13 @@ def delay_spider(spider_name, *args, **kwargs):
     SpiderRunLog.objects.create(spider_name=spider_name, task_id=sync_task.id, param=param)
 
 
-class MyTask(Task, ABC):
+class MyTask(Task):
     def on_success(self, retval, task_id, args, kwargs):
         SpiderRunLog.objects.filter(task_id=task_id).update(status=True, end_time=datetime.datetime.now())
         return super(MyTask, self).on_success(retval, task_id, args, kwargs)
 
 
-class CacheTask(MyTask, ABC):
+class CacheTask(MyTask):
     def on_success(self, retval, task_id, args, kwargs):
         position.views.update_position_visualization_cache(task_id)
         SpiderRunLog.objects.filter(task_id=task_id).update(status=True, end_time=datetime.datetime.now())
--- pyroad/settings.py
+++ pyroad/settings.py
@@ -94,6 +94,7 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4"
         }
     }
 }
